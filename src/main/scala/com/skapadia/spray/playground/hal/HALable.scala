8a4dcd2372718a56895d0f25c0460d567b301f87
skapadia <skapadia@chariotsolutions.com>
2015-03-16 22:04:39 -0400

    Alternate working implementation only case class HALResponse[A] - commented out

--- src/main/scala/com/skapadia/spray/playground/hal/HALable.scala
+++ src/main/scala/com/skapadia/spray/playground/hal/HALable.scala
@@ -4,6 +4,8 @@ import spray.json._
 import spray.httpx.SprayJsonSupport._
 import DefaultJsonProtocol._
 
+// TODO: HAL Links
+
 abstract class HALable[A](implicit writer: JsonWriter[A]) {
   def mainResource: A
   def writeMainResource: JsObject = mainResource.toJson.asJsObject
@@ -26,3 +28,21 @@ object HALable {
     sprayJsonMarshaller[HALable[_]](writer, printer)
 }
 
+
+/* Alternate working implementation with no abstract class
+case class HALResponse[A](mainResource: A, embedded: Map[String, HALResponse[_]])(implicit writer: JsonWriter[A]) {
+  def writeMainResource: JsObject = mainResource.toJson.asJsObject
+  def write: JsObject = new JsObject(writeMainResource.fields + ("embedded" -> writeEmbedded.toJson))
+  def writeEmbedded = embedded.foldLeft(Map[String, JsValue]()) { (map, pair) => map + (pair._1 -> pair._2.write) }
+}
+
+object HALResponse {
+
+  implicit object halableWriter extends RootJsonWriter[HALResponse[_]] {
+    def write(halObj: HALResponse[_]): JsObject = halObj.write
+  }
+
+  implicit def halableMarshallerConverter[T <: HALResponse[_]](implicit writer: RootJsonWriter[HALResponse[_]], printer: JsonPrinter = PrettyPrinter) =
+    sprayJsonMarshaller[HALResponse[_]](writer, printer)
+}
+*/
